da5a3ef127512bc15002399c4460b14a73ccf4a7
oralnayasmazka <50453084+oralnayasmazka@users.noreply.github.com>
2020-12-19 01:24:33 +0300

    fist commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+	$('.questions__item--title').on('click', function () {
+		$('.questions__item').removeClass('questions__item--active');
+		$(this).parent().addClass('questions__item--active');
+	});
+});
+
+window.addEventListener('scroll', function () {
+	let header = document.querySelector('header');
+	let windowPos = window.scrollY > 0;
+	header.classList.toggle('header--active', windowPos);
+});
