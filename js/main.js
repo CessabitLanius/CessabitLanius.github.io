079ac803d52dbeb4f072280e49b14f6051d6cf33
oralnayasmazka <50453084+oralnayasmazka@users.noreply.github.com>
2020-12-21 00:55:46 +0300

    fix js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-$(function () {
-	$('.questions__item--title').on('click', function () {
-		$('.questions__item').removeClass('questions__item--active');
-		$(this).parent().addClass('questions__item--active');
+window.addEventListener('DOMContentLoaded', () => {
+	$(function () {
+		$('.questions__item--title').on('click', function () {
+			$('.questions__item').removeClass('questions__item--active');
+			$(this).parent().addClass('questions__item--active');
+		});
 	});
-});
 
-window.addEventListener('scroll', function () {
-	let header = document.querySelector('header');
-	let windowPos = window.scrollY > 0;
-	header.classList.toggle('header--active', windowPos);
+	window.addEventListener('scroll', function () {
+		let header = document.querySelector('header');
+		let windowPos = window.scrollY > 0;
+		header.classList.toggle('header--active', windowPos);
+	});
 });
